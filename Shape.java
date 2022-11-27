4e4a3a19887f4c45de71aea5c7e4c4c7b40eebff
Gitaramadani24 <114290893+Gitaramadani24@users.noreply.github.com>
2022-11-27 20:08:55 +0700

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_modul11_pbo;
+
+/**
+ *
+ * @author user
+ */
+abstract class Shape {
+private String nm;
+Shape(String nama) {
+nm = nama; }
+public String getName() {
+return nm;
+}
+abstract float getArea();
+
+}
+
+class Circle extends Shape {
+int r;
+Circle(String nama) {
+super(nama);
+r= 3;
+}
+float getArea() {
+float luas;
+luas = (float)(3.14 * r * r);
+return luas;
+}
+}
+
+class Square extends Shape {
+private int sisi;
+Square(String nama) {
+super(nama);
+sisi = 3;
+}
+
+float getArea() {
+float luas;
+luas = sisi * sisi;
+return luas;
+}
+}
